a90e14394b2ced87250c91df81d75c73d33aed3e
Carlo Frinolli <carlo@nois3lab.it>
2012-11-22 09:15:12 +0100

    a huge refactoring

--- drucapify.sh
+++ drucapify.sh
@@ -2,8 +2,9 @@
 
 function usage () {
    cat <<EOF
-Usage: scriptname [-d folder] [-g git repo] [-b git branch] [-i remote ip] [-u user] [-h]
+Usage: scriptname [-d folder] [-p project] [-g git repo] [-b git branch] [-i remote ip] [-u user] [-h]
    -d   the folder you're going to install this
+   -p   project name
    -g   defines the git repo
    -b   defines the git branch
    -i   defines the remote ip
@@ -18,150 +19,198 @@ then
    exit 0
 fi
 
-while getopts p:d:g:b:i:u:h o
+while getopts p:d:g:b:i:u:h:s o
 do  case "$o" in
-    d)  directory="$OPTARG";;
+    p)  APPLICATION="$OPTARG";;
+    d)  PROJECT="$OPTARG";;
+    g)  REMOTE_GIT="$OPTARG";;
+    b)  BRANCH="$OPTARG";;
+    i)  REMOTE_IP="$OPTARG";;
+    u)  USER="$OPTARG";;
+    s)  DOMAIN="$OPTARG";;
 
     esac
 done
 shift $OPTIND-1
 
+
+if [ REMOTE_GIT = "" ]; then
+    REMOTE_GIT=$APPLICATION
+fi
+
+if [ BRANCH = "" ]; then
+    BRANCH="master"
+fi
+
+if [ REMOTE_IP = "" ]; then
+    REMOTE_IP="cap.nois3lab.it"
+fi
+
+if [ USER = "" ]; then
+    USER="nois3lab"
+fi
+
+if [ DOMAIN != "" ]; then
+    DOMAIN=$APPLICATION
+fi
+
 clear
 
-echo -n "INSERT THE PROJECT NAME : "
-read PROJECT
-if [ "$PROJECT" = '' ]; then
-  echo "you must provide a project name"
-  exit 1
-fi
-
-rm -rf /tmp/Drushistrano
-echo "  "
-echo "  "
-echo "---------------------------------------------"
-echo "     Cloning script for the black magic "
-echo "---------------------------------------------"
-echo "  "
-echo "  "
-
-git clone https://github.com/carl0s/Drushistrano.git /tmp/Drushistrano
-echo "  "
-echo "  "
-echo "---------------------------------------------"
-echo "  SETTING UP THE ENVIRONMENT FOR CAPISTRANO "
-echo "---------------------------------------------"
-echo "  "
-echo "  "
-
-
-
-cd $2
-echo -n "You're in "
-echo -n `pwd`
-echo -n " folder"
-echo "  "
-echo "  "
-
-if [ "$3" != "" ]; then
-    REMOTE_GIT=$PROJECT
-else
-    REMOTE_GIT=$3
-fi
-
-if [ "$4" != "" ]; then
-    BRANCH="master"
-else
-    BRANCH=$4
-fi
-
-if [ "$5" != "" ]; then
-    REMOTE_IP="cap.nois3lab.it"
-else
-    REMOTE_IP=$5
-fi
-
-if [ "$6" != "" ]; then
-    USER="nois3lab"
-else
-    USER=$6
-fi
-
-if [ "$7" != "" ]; then
-    DOMAIN=$PROJECT
-else
-    DOMAIN=$7
-fi
-
-
-if [ ! -d "includes" ]; then
-    # Control will enter here if $DIRECTORY doesn't exist.
-    clear
-    echo ":("
-    echo "You're not in a drupal installation"
-    exit 1
-
-else
-
-    echo "  "
-    echo "  "
-    echo "---------------------------------------------"
-    echo "  Preparing environment for cosy deployment  "
-    echo "---------------------------------------------"
-    echo "  "
-    echo "  "
-
-    cp /tmp/Drushistrano/Capfile .
-    cp -R /tmp/Drushistrano/includes/deploy includes/
-    sed -i -bak "s/#APPLICATION_NAME#/$PROJECT/g" includes/deploy/stages.rb
-    sed -i -bak "s/#REMOTE_GIT_PATH#/$REMOTE_GIT/g" includes/deploy/stages.rb
-    sed -i -bak "s/#BRANCH#/$BRANCH/g" includes/deploy/stages.rb
-    sed -i -bak "s/#REMOTE_SERVER_IP#/$REMOTE_IP/g" includes/deploy/stages.rb
-    sed -i -bak "s/#USER#/$USER/g" includes/deploy/stages.rb
-    sed -i -bak "s/#DOMAIN#/$DOMAIN/g" includes/deploy/stages.rb
-fi
-
-
-
-
-if [ ! -d "sites" ]; then
-    # Control will enter here if $DIRECTORY doesn't exist.
-    clear
-    echo ":("
-    echo "You're not in a drupal installation"
+if [ ! -d $PROJECT"/sites" ]; then
+    echo "$PROJECT/sites"
+    echo "This is for Drupal projects, please provide a 'sites' folder"
     exit 1
 else
-    cp .htaccess htaccess
-    cd sites
-    echo "  "
-    echo "  "
-    echo "---------------------------------------------"
-    echo "  Removing old default folder from drupal "
-    echo "---------------------------------------------"
-    echo "  "
-    echo "  "
-    rm -rf default
-    cp -R /tmp/Drushistrano/default-development .
-    cp -R /tmp/Drushistrano/default-production .
-    echo "  "
-    echo "  "
-    echo "---------------------------------------------"
-    echo "         Creating the right domain "
-    echo "---------------------------------------------"
-    echo "  "
-    echo "  "
 
-    cp -R /tmp/Drushistrano/default $PROJECT
+    rm -rf /tmp/Drushistrano
 
     echo "  "
     echo "  "
     echo "---------------------------------------------"
-    echo "         Symlinking for the win "
+    echo "   Checking if this is already drucapified   "
     echo "---------------------------------------------"
     echo "  "
     echo "  "
 
-    ln -s $PROJECT default
+    if [ -d "includes/deploy" ]; then
+        # Control will enter here if $DIRECTORY doesn't exist.
+        clear
+        echo "Cheers! the project is already drucapified!"
+        exit 1
+
+    else
+        echo "[ok!]"
+        echo "  "
+        echo "  "
+        echo "---------------------------------------------"
+        echo "     Cloning script for the black magic "
+        echo "---------------------------------------------"
+        echo "  "
+        echo "  "
+
+        git clone https://github.com/carl0s/Drushistrano.git /tmp/Drushistrano
+        echo "[ok!]"
+        echo "  "
+        echo "  "
+        echo "---------------------------------------------"
+        echo "  SETTING UP THE ENVIRONMENT FOR CAPISTRANO "
+        echo "---------------------------------------------"
+        echo "  "
+        echo "  "
+
+        cd $APPLICATION
+        rm -rf .git
+        rm -rf .gitignore
+
+        echo "[ok!]"
+        echo -n "You're in "
+        echo -n `pwd`
+        echo -n " folder"
+        echo "  "
+        echo "  "
+
+
+
+        echo "  "
+        echo "  "
+        echo "---------------------------------------------"
+        echo "  Preparing environment for cosy deployment  "
+        echo "---------------------------------------------"
+        echo "  "
+        echo "  "
+
+        cp /tmp/Drushistrano/Capfile .
+        cp -R /tmp/Drushistrano/includes/deploy includes/
+        sed -i -bak "s/#APPLICATION_NAME#/$APPLICATION/g" includes/deploy/stages.rb
+        sed -i -bak "s/#REMOTE_GIT_PATH#/$REMOTE_GIT/g" includes/deploy/stages.rb
+        sed -i -bak "s/#BRANCH#/$BRANCH/g" includes/deploy/stages.rb
+        sed -i -bak "s/#REMOTE_SERVER_IP#/$REMOTE_IP/g" includes/deploy/stages.rb
+        sed -i -bak "s/#USER#/$USER/g" includes/deploy/stages.rb
+        sed -i -bak "s/#DOMAIN#/$DOMAIN/g" includes/deploy/stages.rb
+
+        cp .htaccess htaccess
+        cd $PROJECT/sites
+        echo "[ok!]"
+        echo "  "
+        echo "  "
+        echo "---------------------------------------------"
+        echo "  Removing old default folder from drupal "
+        echo "---------------------------------------------"
+        echo "  "
+        echo "  "
+        rm -rf default
+        cp -R /tmp/Drushistrano/default-development .
+        cp -R /tmp/Drushistrano/default-production .
+
+        echo "  "
+        echo "executed "
+        echo -n cp -R /tmp/Drushistrano/default-production .
+        echo "  "
+        echo "executed "
+        echo -n cp -R /tmp/Drushistrano/default-development .
+
+        echo "                      [ok!]"
+        echo "  "
+        echo "  "
+        echo "---------------------------------------------"
+        echo "         Creating the right domain "
+        echo "---------------------------------------------"
+        echo "  "
+        echo "  "
+
+        cp -R /tmp/Drushistrano/default $APPLICATION
+        mkdir $APPLICATION/files
+
+        echo "  "
+        echo "executed "
+        echo -n cp -R /tmp/Drushistrano/default $APPLICATION
+        echo "                     [ok!]"
+
+
+
+        echo "  "
+        echo "  "
+        echo "---------------------------------------------"
+        echo "         Symlinking for the win "
+        echo "---------------------------------------------"
+        echo "  "
+        echo "  "
+
+        ln -s $APPLICATION default
+
+        echo "  "
+        echo "executed "
+        echo -n ln -s $APPLICATION default
+        echo "                  [ok!]"
+
+        echo "  "
+        echo "  "
+        echo "---------------------------------------------"
+        echo "         Initialized Git environment "
+        echo "---------------------------------------------"
+        echo "  "
+        echo "  "
+
+        git init $PROJECT/.
+        cp /tmp/Drushistrano/gitignore .gitignore
+        git add .
+        git add -f $PROJECT/sites/$APPLICATION/settings.development.php
+        git add -f $PROJECT/sites/$APPLICATION/settings.production.php
+        git commit -am "Initial commit"
+        git remote add origin git@git.nois3lab.it:$REMOTE_GIT.git
+        echo "[ok!]"
+
+    fi
+
 fi
+
+
+
+
+
+
+
+
 
 
 
